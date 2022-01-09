36f93efd5683c3479c5012203d4bf40f3d4dfb94
Yannick Raats <yannickraats@gmail.com>
2022-01-09 14:35:04 +0100

    navbar styling

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,13 +1,16 @@
 import { Link } from "react-router-dom";
 
 //styles
-import "./Navbar.css";
+import '../navbar/Navbar.css'
 
 export default function Navbar() {
   return (
     <div className="navbar">
-      <Link>
+      <Link to="/" className="brand">
         <h1>Cooking directory</h1>
+      </Link>
+      <Link to="/create">
+          Create recipe
       </Link>
     </div>
   );
