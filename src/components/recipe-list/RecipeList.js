5bb943b64f8311b207a32e17c7991c38b911d4ff
Yannick Raats <yannickraats@gmail.com>
2022-01-09 16:09:56 +0100

    recipelist component + fetch

--- src/components/recipe-list/RecipeList.js
+++ src/components/recipe-list/RecipeList.js
@@ -1,3 +1,13 @@
-
 // styles
-import './RecipeList.css'
+import "./RecipeList.css";
+
+export default function RecipeList({ recipes }) {
+
+  return (
+    <div className="recipe-list">
+      {recipes.map((recipe) => (
+        <h2 key={recipe.id}>{recipe.title}</h2>
+      ))}
+    </div>
+  );
+}
